ca6aa66705298ca2659935cceff7feb98519e2ce
luisalvs <luisalvst@gmail.com>
2025-09-10 22:46:43 -0300

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,8 +1,24 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://www.pythonscraping.com/pages/warandpeace.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-nome_lista = bs.find_all('span', {'class': 'green'})
-for name in nome_lista:
-    print(name.get_text())
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+# print(soup.prettify())
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+for link in soup.find_all('a'):
+    print(link.get('href'))
